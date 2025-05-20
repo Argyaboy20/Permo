71317de945d3b85498e438bffccc5ed08372f057
Argyaboy20 <smpn21985@gmail.com>
2025-05-20 15:02:06 +0700

    memperbaiki filterpipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,29 +5,36 @@ import { IonicStorageModule } from '@ionic/storage-angular';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+
+import { Tab2Page } from './tab2/tab2.page';
 import { HttpClientModule } from '@angular/common/http';
 import { PostProvider } from '../provider/post-provider';
 import { FilterPipe } from './filter.pipe';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { KamusPageRoutingModule } from './kamus/kamus-routing.module';
+import { KamusPage } from './kamus/kamus.page';
+import { IlmutanahPageRoutingModule } from './ilmutanah/ilmutanah-routing.module';
+import { IlmutanahPage } from './ilmutanah/ilmutanah.page';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FilterPipe
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
     IonicModule.forRoot(),
     AppRoutingModule,
     HttpClientModule,
+    KamusPageRoutingModule,
+    IlmutanahPageRoutingModule,
+    ReactiveFormsModule,
     IonicStorageModule.forRoot()
+
   ],
   providers: [
     PostProvider,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
   ],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent, Tab2Page, FilterPipe, KamusPage, IlmutanahPage],
 })
 export class AppModule {}
